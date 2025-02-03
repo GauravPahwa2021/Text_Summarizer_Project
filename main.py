1105c7ff4880e4bdfc80e8f37f240ea2b9ccfd86
GauravPahwa143 <gauravpahwa2020@gmail.com>
2025-02-03 20:45:16 +0530

    data ingestion completed

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+from src.textSummarizer.logging import logger
+from src.textSummarizer.exception import CustomException
+from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
+
+
+STAGE_NAME="Data Ingestion stage"
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    data_ingestion_pipeline=DataIngestionTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    raise CustomException(e,sys)
