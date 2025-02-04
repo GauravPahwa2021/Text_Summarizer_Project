fc2d4f19a4d40f25bb0b66e5632e6e49d03bac98
GauravPahwa143 <gauravpahwa2020@gmail.com>
2025-02-04 16:25:31 +0530

    data transformation stage added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from src.textSummarizer.logging import logger
 from src.textSummarizer.exception import CustomException
 from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.textSummarizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
 
 
 STAGE_NAME="Data Ingestion stage"
@@ -9,6 +10,16 @@ try:
     logger.info(f"stage {STAGE_NAME} initiated")
     data_ingestion_pipeline=DataIngestionTrainingPipeline()
     data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    raise CustomException(e,sys)
+
+
+STAGE_NAME="Data Transformation stage"
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    data_ingestion_pipeline=DataTransformationTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_transformation()
     logger.info(f"Stage {STAGE_NAME} Completed")
 except Exception as e:
     raise CustomException(e,sys)
